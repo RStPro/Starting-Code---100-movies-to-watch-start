798c803d6e2549b5c5faf7c2be455b428751d430
RstProfessional <rsantos.professional@gmail.com>
2023-03-03 19:38:06 +0000

    add soupe

--- main.py
+++ main.py
@@ -5,4 +5,20 @@ URL = "https://web.archive.org/web/20200518073855/https://www.empireonline.com/m
 
 # Write your code below this line 👇
 
+response =  requests.get(URL)
+web_page = response.text
+
+soup = BeautifulSoup(web_page, "html.parser")
+
+movies = []
+
+movies_names_data = soup.find_all(name="h3", class_="title")
+
+for movie in movies_names_data:
+    name = movie.getText()
+    movies.append(name)
+    # ordered_list = movies.reverse()
+
+print(mo)
+
 
